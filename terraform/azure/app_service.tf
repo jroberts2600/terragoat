58f43e2d2881498a6a5f7aedb609b76225816623
Jason Roberts <51415896+jroberts2600@users.noreply.github.com>
2021-04-13 09:51:28 -0500

    Update app_service.tf

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -9,7 +9,7 @@ resource azurerm_app_service_plan "example" {
   }
 }
 
-resource azurerm_app_service "app-service1" {
+resource azurerm_app_service "app-service1.1" {
   app_service_plan_id = azurerm_app_service_plan.example.id
   location            = var.location
   name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
